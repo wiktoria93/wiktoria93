3bd04bfb826ae91a2651a00af445c2c1d4619fca
Wiktoria Kałka <33886@student.ue.poznan.pl>
2016-03-23 13:09:30 +0100

    obsluga bledu dodana

--- kalkulator.py
+++ kalkulator.py
@@ -2,7 +2,10 @@ def dodawanie (a, b):
 
 	return a+b
 
+try:	
 a = int(input("podaj pierwsza liczbe"))
 b = int(input("podaj druga liczbe"))
 print(dodawanie(a, b))
+except ValueError as ve:
+	print("wprowadzono bledne dane, koncze dzialanie")
 input()
